eb90af9ad2c50094f2541d95e301a44fc40fbc4c
AndreMachado-Varicent <amachado@varicent.com>
2021-11-29 08:57:53 -0500

    add comments

--- powershell.sh
+++ powershell.sh
@@ -1,3 +1,5 @@
+# kill all node processes
 Do { Stop-Process -Name node; } Until ($? -ne "True")
 
+# open Windows Terminal tabs
  wt  -w _quake -p "bash"`; split-pane -p "bash" `; split-pane -p "bash"`; nt -p "bash"`; split-pane -d Varicent -p "bash"
