68d5569e2729d729396b189b1131cecb32312d0a
Andre Machado <amachado@varicent.com>
2026-03-04 11:19:07 -0500

    ignore test files

--- bash/largeFiles.sh
+++ bash/largeFiles.sh
@@ -10,4 +10,5 @@ min_lines=2000
 # Use find to get all files in the directory and subdirectories
 # Use wc -l to count the number of lines in each file
 # Use awk to print the filename if the number of lines is greater than min_lines
-find "$dir" -name "*.ts*" -type f -exec awk -v min_lines="$min_lines" 'NR > min_lines { print FILENAME; exit }' {} \;
+# Exclude test files
+find "$dir" -name "*.ts*" -not -name "*.test*.ts*" -type f -exec awk -v min_lines="$min_lines" "NR > min_lines { print FILENAME; exit }" {} \;
